a8b75b95c925cb9bf58905889ca7308439ad8e99
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2025-04-04 19:27:55 -0300

    .

--- js/script.js
+++ js/script.js
@@ -4,3 +4,28 @@ const multiplicationInput = document.querySelector("#multiplicator");
 const multiplicationTitle = document.querySelector("#multiplication-title span");
 const multiplicationTable = document.querySelector("#multiplication-operations");
 
+const erik=10;
+
+const createTable = (number,multiplicatorNumber)=>{
+    multiplicationTable.innerHTML = "";
+
+    for(i = 1; i<=multiplicatorNumber;i++){
+
+        const result = number*i;
+
+        const template = `<div class="row">
+            <div class="operation">${number} x ${i}</div>
+            <div class="result">${result}</div>
+        </div>`;
+
+        const parser = new DOMParser();
+        const htmlTemplate = parser.parseFromString(template,"text/html");
+
+        const row = htmlTemplate.querySelector(".row");
+        multiplicationTable.appendChild(row);
+
+    }
+
+    multiplicationTitle.innerText = number;
+
+}
