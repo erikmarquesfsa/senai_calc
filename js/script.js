a8a68787dd4164b8371257627b14231d72add989
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2025-04-03 21:19:45 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-const multiplicationForm = 
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+const multiplicationTitle = document.querySelector("#multiplication-title span");
+const multiplicationTable = document.querySelector("#multiplication-operations");
+
